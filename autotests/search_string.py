385836eeed008d106d97c592ad2c58263c208ddd
Vladislav <st091242@student.spbu.ru>
2022-12-10 12:11:43 +0300

    test search string optimization

--- autotests/search_string.py
+++ autotests/search_string.py
@@ -1,20 +1,27 @@
+from telnetlib import EC
+
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 link = "http://localhost:4200/"
 browser = webdriver.Chrome()
+browser.maximize_window()
 browser.get(link)
 
-search_string = browser.find_element(By.XPATH, "/html/body/town-finder/div/app-city-list/div/form/input")
+search_string = browser.find_element(By.CSS_SELECTOR, "input[class*=ng-untouched]")
 search_string.send_keys("Москва")
-knop = browser.find_element(By.XPATH, "/html/body/town-finder/div/app-city-list/div/form/button/span").click()
-time.sleep(2)
-test = browser.find_element(By.XPATH, "/html/body/town-finder/div/app-city-list/div/div/a/div[2]/h2").text
+
+
+knop = browser.find_element(By.CSS_SELECTOR, "span[class*=material-symbols-outlined]").click()
+
+
+test = WebDriverWait(browser,10).until(
+        EC.visibility_of_element_located((By.XPATH, "/html/body/town-finder/div/app-city-list/div/div/a/div[2]/h2"))).text
+
 test_1 = "Москва"
-if test == test_1:
-        print("Verification was successful")
-else:
-        print("Verification failed")
+assert test == test_1, f"Test not successful {test}"
+
 browser.quit()
